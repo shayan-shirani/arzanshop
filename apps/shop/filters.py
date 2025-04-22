b8d5abaee53ba253ac53f99cb90dcfde2b99b849
shayan-shirani <shayan.sh350@gmail.com>
2025-04-22 19:06:58 +0330

    added price filtering

--- apps/shop/filters.py
+++ apps/shop/filters.py
@@ -4,6 +4,8 @@ from django_filters import rest_framework as filters
 
 class ProductFilter(filters.FilterSet):
     vendor = filters.NumberFilter(field_name='vendor__id', lookup_expr='exact')
+    min_price = filters.NumberFilter(field_name='price', lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name='price', lookup_expr='lte')
     category = filters.CharFilter(method='filter_by_category')
 
     def filter_by_category(self, queryset, name, value):
